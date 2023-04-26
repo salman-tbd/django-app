91af5f0d254d139e401a811d74f1fa773f0d436f
salman <salman@technobitsdigital.com>
2023-04-26 20:04:51 +0530

    new app

--- app_system/admin.py
+++ app_system/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from app_system.models import Tbl_FieldTask_Purpose_Master, Tbl_Tasks
+
+admin.site.register(Tbl_FieldTask_Purpose_Master)
+admin.site.register(Tbl_Tasks)
